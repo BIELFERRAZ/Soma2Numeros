b2e292c4e9b1f2ba73e68fc6fad138811e0c5508
matheus <matheusgabrielferraz0802@gmail.com>
2022-03-18 20:20:22 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int n1 , n2 , resultado;
+Console.WriteLine("Cálculo da soma entre dois Números.\n");
+
+Console.Write ("Digite o primeiro número:");
+n1 = Convert.ToInt32(Console.ReadLine ()!);
+
+Console.Write("Digite o segundo número: ");
+ n2 = Convert.ToInt32(Console.ReadLine ()!);
+
+ resultado = n1 + n2; 
+
+ Console.WriteLine ($"Soma: {resultado}" ); 
